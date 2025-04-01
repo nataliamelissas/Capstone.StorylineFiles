969450e0cd7c095d0c84551031f8323946901045
Natalia Soto <nataliamelissas@gmail.com>
2025-03-31 23:13:29 -0600

    added welcome video.

---
+++ Extracted/Welcome/story_content/6paxse0WPaK_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.97.34031.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:03.640%0D%0AWelcome%20to%20the%20module%20on%20designing%20effective%20online%20classrooms!%0D%0A%0D%0A"}')
